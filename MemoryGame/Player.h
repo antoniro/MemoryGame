c4562f4e012203b766f6790dcfb3f447c0c5a259
Oscar Anthony Gbegan <anthony.gbegan@gmail.com>
2018-12-05 20:18:51 -0500

    Add declarations of attributes and functions in Player.h

--- MemoryGame/Player.h
+++ MemoryGame/Player.h
@@ -1,8 +1,31 @@
 #pragma once
+
+#include <string>
+#include <iostream>
+
+#include "Reward.h"
+
+enum class Side { Top, Bottom, Left, Right };
+
 class Player
 {
 public:
 	Player();
 	~Player();
-};
+	std::string getName() const;
+	void setActive(bool);
+	bool isActive() const;
+	int getNRubies() const;
+	void addReward(Reward const &);
+	void setDisplayMode(bool endOfGame);
+	Side getSide() const;
+	void setSide(Side);
+	friend std::ostream & operator << (std::ostream &, Player const &);
 
+private:
+	std::string _name;
+	Side _side;
+	int _nRubies;
+	bool _isActive;
+
+};
