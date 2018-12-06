c62d89ebf62bbcf24b82f08969709c434597d6da
Oscar Anthony Gbegan <anthony.gbegan@gmail.com>
2018-12-05 21:48:19 -0500

    Add attributes and function declaration in Card.h

--- MemoryGame/Card.h
+++ MemoryGame/Card.h
@@ -1,8 +1,23 @@
 #pragma once
+
+#include <string>
+#include <iostream>
+
+enum class FaceAnimal { Crab, Penguin, Octopus, Turtle, Walrus };
+enum class FaceBackground { Red, Green, Purple, Blue, Yellow };
+
 class Card
 {
 public:
 	Card();
 	~Card();
-};
+	int getNRows();
+	std::string operator () (int row);
+	operator FaceAnimal();
+	operator FaceAnimal();
 
+private:
+	Card(FaceAnimal animal, FaceBackground background);
+	FaceAnimal _animal;
+	FaceBackground _background;
+};
